25ea322ebc2ec45bb7e36b292a080b5ad93d0f63
関倖太郎 <kotaro.seki1216@gmail.com>
2021-12-05 17:23:41 +0900

    * AppIconを追加

---
+++ Common/Extension/UIViewController+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Extension.swift
+//  RunnersHigh
+//
+//  Created by kotaro-seki on 2021/12/05.
+//
+
+import Foundation
+import UIKit
+
+public extension UIViewController {
+    typealias Creator = (NSCoder) -> UIViewController?
+}
